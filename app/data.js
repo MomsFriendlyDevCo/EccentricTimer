aa10ac2f7a5d495bee895df916172ec42dbbdaaf
Matt Carter <m@ttcarter.com>
2014-08-13 08:54:08 +1000

    Added compressed schedule

--- app/data.js
+++ app/data.js
@@ -10,17 +10,24 @@ app.run(function($rootScope) {
 	};
 
 	$rootScope.timers = [
+		/*
 		{
-			id: 'tennisElbow',
-			title: 'Tennis Elbow',
-			script: [
-				/**
-				* {
+			id: String // Unique ID of this timer (used for URL slug)
+			title: String // Human friendly name of the timer profile
+			script: [ // Array of script items
+				{
 					title: String // The display title of the item
 					time: Int // Time in seconds to display
 					say: Bool|String // If true the title will be said via TTS, if a string it will be passed to TTS
 				{
-				*/
+			]
+		}
+		*/
+
+		{
+			id: 'tennisElbow',
+			title: 'Tennis Elbow',
+			script: [
 				{
 					title: 'Preperation',
 					time: 5 * 1000,
@@ -86,6 +93,87 @@ app.run(function($rootScope) {
 				{
 					title: 'Rest',
 					time: 60 * 1000,
+					say: true
+				},
+				{
+					title: 'Left arm stress #4',
+					time: 60 * 1000,
+				},
+				{
+					title: 'Finished',
+					say: true
+				}
+			]
+		},
+		{
+			id: 'tennisElbowCompressed',
+			title: 'Tennis Elbow (Alternating)',
+			script: [
+				{
+					title: 'Preperation',
+					time: 5 * 1000,
+				},
+				{
+					title: 'Right arm stress #1',
+					time: 60 * 1000,
+				},
+				{
+					title: 'Switch arms',
+					time: 10 * 1000,
+					say: true
+				},
+				{
+					title: 'Left arm stress #1',
+					time: 60 * 1000,
+				},
+				{
+					title: 'Switch arms',
+					time: 10 * 1000,
+					say: true
+				},
+				{
+					title: 'Right arm stress #2',
+					time: 60 * 1000,
+				},
+				{
+					title: 'Switch arms',
+					time: 10 * 1000,
+					say: true
+				},
+				{
+					title: 'Left arm stress #2',
+					time: 60 * 1000,
+				},
+				{
+					title: 'Switch arms',
+					time: 10 * 1000,
+					say: true
+				},
+				{
+					title: 'Right arm stress #3',
+					time: 60 * 1000,
+				},
+				{
+					title: 'Switch arms',
+					time: 10 * 1000,
+					say: true
+				},
+				{
+					title: 'Left arm stress #3',
+					time: 60 * 1000,
+				},
+				{
+					title: 'Switch arms',
+					time: 10 * 1000,
+					say: true
+				},
+				{
+					title: 'Right arm stress #4',
+					time: 60 * 1000,
+				},
+				{
+					title: 'Switch arms',
+					time: 10 * 1000,
 					say: true
 				},
 				{
