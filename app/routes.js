447f0f18c38908b6f6f258726f022e6eb70ffd2c
Matt Carter <m@ttcarter.com>
2014-08-13 08:54:23 +1000

    BUGFIX: Various minor bug fixes

--- app/routes.js
+++ app/routes.js
@@ -11,7 +11,7 @@ app.config(function($routeProvider, $locationProvider) {
 		.when('/error', {
 			templateUrl: "/templates/error.html",
 			animateIn: 'fadeInUpBig'
-		});
+		})
 		.when('/settings', {
 			templateUrl: "/templates/settings.html",
 			animateIn: 'fadeInUpBig',
