de2b42701cde74397664c9b52638f67d84005b09
Matt Carter <m@ttcarter.com>
2014-10-15 13:59:07 +1000

    Added 60s weights

--- app/data.js
+++ app/data.js
@@ -446,6 +446,90 @@ app.run(function($rootScope) {
 					countdown: false
 				}
 			]
+		},
+		{
+			id: 'tennisElbow3-60s',
+			title: 'Tennis Elbow Session 3 (60s)',
+			description: '5kg Weight holding exorcizes, twice a day with 60 second intervals',
+			routineTime: '14 days',
+			script: [
+				{
+					title: 'Preperation',
+					time: 5 * 1000,
+				},
+				{
+					title: 'Right arm weights #1',
+					time: 60 * 1000,
+				},
+				{
+					title: 'Switch to left arm',
+					time: 5 * 1000,
+					say: true
+				},
+				{
+					title: 'Left arm weights #1',
+					time: 60 * 1000,
+				},
+				{
+					title: 'Switch to right arm',
+					time: 5 * 1000,
+					say: true
+				},
+				{
+					title: 'Right arm weights #2',
+					time: 60 * 1000,
+				},
+				{
+					title: 'Switch to left arm',
+					time: 5 * 1000,
+					say: true
+				},
+				{
+					title: 'Left arm weights #2',
+					time: 60 * 1000,
+				},
+				{
+					title: 'Switch to right arm',
+					time: 5 * 1000,
+					say: true
+				},
+				{
+					title: 'Right arm weights #3',
+					time: 60 * 1000,
+				},
+				{
+					title: 'Switch to left arm',
+					time: 5 * 1000,
+					say: true
+				},
+				{
+					title: 'Left arm weights #3',
+					time: 60 * 1000,
+				},
+				{
+					title: 'Switch to right arm',
+					time: 5 * 1000,
+					say: true
+				},
+				{
+					title: 'Right arm weights #4',
+					time: 60 * 1000,
+				},
+				{
+					title: 'Switch to left arm',
+					time: 5 * 1000,
+					say: true
+				},
+				{
+					title: 'Left arm weights #4',
+					time: 60 * 1000,
+				},
+				{
+					title: 'Finished',
+					say: true,
+					countdown: false
+				}
+			]
 		}
 	];
 });
